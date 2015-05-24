f3b62ba108a044863b5d187b3cfe9fa37bd95a70
Michael <russellyates2013@gmail.com>
2015-05-25 03:51:21 +1200

    Reworked views to use product information from the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 growth_hacking = Product.create(title: "Growth Hacking", 
  subtitle: "Crash Course", author: "Mattan Griffel", 
  length: "30 minutes", author_image_name: "teacher-image.jpg", 
- price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments-2003/07+Rm+16+Jackson.mp3", 
  details: "You'll get one video", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
  <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
@@ -27,7 +27,7 @@ growth_hacking = Product.create(title: "Growth Hacking",
 growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly", 
  subtitle: "Ongoing Course", author: "Mattan Griffel", 
  length: "6 months", author_image_name: "teacher-image.jpg", 
- price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments-2003/07+Rm+16+Jackson.mp3", 
  details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
  <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
